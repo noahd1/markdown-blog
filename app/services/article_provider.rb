dc7d0490a193600908a7e17f7590e0898f422149
mc388 <marvin.caspar@gmail.com>
2015-11-12 21:49:25 +0100

    Update article_provider.rb

--- app/services/article_provider.rb
+++ app/services/article_provider.rb
@@ -1,7 +1,7 @@
-# Public: ArticleProvider creates a Article object from a markdown file
+# ArticleProvider creates a Article object from a markdown file
 class ArticleProvider
 
-  # Public: Initialize the ArticleProvider class
+  # Initialize the ArticleProvider class
   #
   # filename - The filename for creating a new Article object
   #
@@ -12,43 +12,49 @@ class ArticleProvider
     @filename = filename
   end
 
-  # Public: Creates the Article from a given filename
+  # Creates the Article from a given filename
   #
   # Returns a new Article object
   def execute
     begin
+      filename = File.basename(@filename, ".*")
       markdown = File.read(@filename)
       parser = MarkdownParser.new markdown
       data = parser.execute
       html = data.output
       metadata = data.metadata
 
-      filename = File.basename(@filename, ".*")
+      tags = []
+      unless metadata.empty?
+        title = metadata[:title]
+        author = metadata[:author]
+        tags = get_tags(metadata)
+      end
 
-      # Create or edit article model
       article = Article.find_or_create_by(filename: filename)
-      article.title = metadata[:title]
-      article.slug = article.title.downcase.tr(" ", "-")
+      article.title = title
+      article.slug = article.title.downcase.gsub(" ", "-")
       article.markdown = markdown
       article.content = html
-      article.author = metadata[:author]
-      article.tags = get_tags(metadata)
-  
-      article.save
-      article
-    rescue => e
+      article.author = author
+      article.tags = tags
+
+     article
+    rescue Exception => e
       raise "Can't read file! #{ e.message }"
     end
   end
 
   private
 
-  # Private: Get used Tags for a Article. If a use Tag not exists,
+  # Get used Tags for a Article. If a use Tag not exists,
   # it will be created
   #
   # metadata - The metadata from a markdown file
   #
   # Returns used Tags
+  #
+  #   ArticleProvider.new "/path/to/markdown/fil.md"
   def get_tags(metadata)
     unless metadata[:tags].nil? && metadata[:tags].empty?
       tags = metadata[:tags]
